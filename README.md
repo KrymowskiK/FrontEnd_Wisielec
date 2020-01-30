340e10be51b94e91f607738d6b1ee2577593027e
KrymowskiK <57898043+KrymowskiK@users.noreply.github.com>
2020-01-30 18:53:57 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+# Gra w wisielca
+
+EN:
+The first website created as a hobby to deepen the knowledge and learning of used technologies.
+
+PL:
+Pierwsza strona utworzona hobbystycznie, w celu pogłębiania wiedzy i nauki używanych technologii.
+
+
+## General info
+
+EN:
+Hangman game - a popular game of guessing the password.
+If we guess, we discover the letter in the password and we are closer to solving the riddle.
+If we don't guess, more gallows are added.
+The game ends with guessing the password (all letters are revealed - winning) or when the image of the hangman appears
+it will appear to us in full glory (9 incorrect indications of letters - lost).
+
+PL:
+Gra w wisielca - popularna gra polegająca na odgadywaniu hasła. 
+Jeśli odgadniemy, odkrywa nam się litera w haśle, dzięki czemu jesteśmy bliżej rozwiązania zagadki. 
+Jeśli nie odgadniemy, dodawane są kolejne elementy szubienicy. 
+Gra kończy się odgadnięciem hasła (odkrycie wszystkich liter - wygrana) lub kiedy obraz wisielca 
+ukaże nam się w pełnej okazałości (9 błędnych wskazań liter - przegrana).
+
+
+## Technologies
+
+Project is created with:
+* HTML
+* CSS
+* JavaScript
+* GIMP
---
+++ szubienica.js
@@ -0,0 +1,153 @@
+const lista_hesel = ["Jak sobie pościelisz, tak się wyśpisz", 
+					 "Gdyby kózka nie skakała, to by nóżki nie złamała", 
+					 "Bez pracy nie ma kołczy", 
+					 "Raz na wozie, raz pod wozem", 
+					 "Darowanemu koniowi w zęby się nie zagląda",
+					 "Fortuna kołem się toczy",
+					 "Nie chwal dnia przed zachodem słońca",
+					 "Lepszy wróbel w garści niż gołąb na dachu",
+					 "Apetyt rośnie w miarę jedzenia",
+					 "Co ma wisieć, nie utonie",
+					 "Dzieci i ryby głosu nie mają",
+					 "Grosz do grosza, a będzie kokosza",
+					 "Łaska pańska na pstrym koniu jeździ",
+					 "Czego oczy nie widzą, tego sercu nie żal",
+					 "Człowiek człowiekowi wilkiem"];
+
+function losowe_haslo(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+
+var haslo = losowe_haslo(lista_hesel);
+haslo = haslo.toUpperCase();
+
+var dlugosc = haslo.length;
+var ile_bledow = 0;
+
+var haslo1 = "";
+
+for (i=0; i<dlugosc; i++)
+{
+	if (haslo.charAt(i)==" ") haslo1 = haslo1 + " ";
+	else if (haslo.charAt(i)==",") haslo1 = haslo1 + ",";
+	else if (haslo.charAt(i)=="-") haslo1 = haslo1 + "-";
+	else if (haslo.charAt(i)==".") haslo1 = haslo1 + ".";
+	else if (haslo.charAt(i)==":") haslo1 = haslo1 + ":";
+	else haslo1 = haslo1 + "-";
+}
+
+function wypisz_haslo()
+{
+	document.getElementById("plansza").innerHTML = haslo1;
+}
+
+window.onload = start;
+
+var litery = new Array(35);
+
+litery[0] = "A";
+litery[1] = "Ą";
+litery[2] = "B";
+litery[3] = "C";
+litery[4] = "Ć";
+litery[5] = "D";
+litery[6] = "E";
+litery[7] = "Ę";
+litery[8] = "F";
+litery[9] = "G";
+litery[10] = "H";
+litery[11] = "I";
+litery[12] = "J";
+litery[13] = "K";
+litery[14] = "L";
+litery[15] = "Ł";
+litery[16] = "M";
+litery[17] = "N";
+litery[18] = "Ń";
+litery[19] = "O";
+litery[20] = "Ó";
+litery[21] = "P";
+litery[22] = "Q";
+litery[23] = "R";
+litery[24] = "S";
+litery[25] = "Ś";
+litery[26] = "T";
+litery[27] = "U";
+litery[28] = "V";
+litery[29] = "W";
+litery[30] = "X";
+litery[31] = "Y";
+litery[32] = "Z";
+litery[33] = "Ź";
+litery[34] = "Ż";
+
+function start()
+{
+	
+	var tresc_diva = "";
+	
+	for(i=0; i<=34; i++)
+	{
+		var element = "lit"+i;
+		tresc_diva = tresc_diva + '<div class="litera" onclick="sprawdz('+i+')" id="'+element+'">'+litery[i]+'</div>';
+		if ((i+1)%7==0) tresc_diva = tresc_diva + '<div style="clear:both"></div>'
+	}
+	
+	document.getElementById("alfabet").innerHTML = tresc_diva;
+	
+	wypisz_haslo();
+}
+
+String.prototype.ustawZnak = function(miejsce, znak)
+{
+	if (miejsce > this.length - 1) return this.toString();
+	else return this.substr(0, miejsce) + znak + this.substr(miejsce + 1);
+}
+
+function sprawdz(nr)
+{
+	var trafiona = false;
+	
+	for (i=0; i<dlugosc; i++)
+	{
+		if (haslo.charAt(i) == litery[nr])
+		{
+			haslo1 = haslo1.ustawZnak(i,litery[nr]);
+			trafiona = true;
+		}
+	}
+	
+	if(trafiona == true)
+	{
+		var element = "lit" + nr;
+		document.getElementById(element).style.background = "#003300";
+		document.getElementById(element).style.color = "#00C000";
+		document.getElementById(element).style.border = "solid #00C000";
+		document.getElementById(element).style.cursor = "default";
+		
+		wypisz_haslo();
+	}
+	else
+	{
+		var element = "lit" + nr;
+		document.getElementById(element).style.background = "#330000";
+		document.getElementById(element).style.color = "#C00000";
+		document.getElementById(element).style.border = "solid #C00000";
+		document.getElementById(element).style.cursor = "default";
+		document.getElementById(element).setAttribute("onclick",";");
+		
+		//bledy - nietrafione litery
+		ile_bledow++;
+		var obraz = "obrazy/szubienica" + ile_bledow + ".jpg";
+		document.getElementById("szubienica").innerHTML = '<img src="' + obraz + '"alt="" />'
+	}
+	
+	//wygrana
+	if (haslo == haslo1)
+	document.getElementById("alfabet").innerHTML = "WYGRANA! <br/> Podano prawidłowe hasło: " + haslo + '<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+	
+	//przegrana
+	if (ile_bledow>=9)
+	document.getElementById("alfabet").innerHTML = "PRZEGRANA! <br/> Podano złe hasło! <br/> Prawidłowe hasło: " + haslo + '<br/><br/><span class="reset" onclick="location.reload()">JESZCZE RAZ?</span>';
+}
